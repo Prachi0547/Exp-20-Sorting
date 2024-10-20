8060572be53e344817e2b6d164b7d06bbd63075c
Prachi0547 <prachi0547@gmail.com>
2024-10-20 20:56:11 +0530

    Add files via upload

---
+++ Exp 20b.cpp
@@ -0,0 +1,31 @@
+//Insertion Sorting
+
+#include <iostream>
+using namespace std;
+
+void insertionsort(int arr[], int n){
+    for (int i = 1; i<n ; i++){
+        int key = arr [i];
+        int j =i-1;
+
+        while (j>= 0 &&  arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1]= key;
+    }
+}
+int main(){
+    int arr[]= {7, 19, 3, 23, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout<< "Original array: ";
+    for (int i =0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    insertionsort(arr,n);
+    cout<< "\nSorted array: ";
+    for (int i = 0; i< n ; i++){
+        cout << arr[i]<<" ";
+    }
+    return 0;
+    }
